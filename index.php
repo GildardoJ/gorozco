f980a1b89d6e5c69d457179219bcf502bcfe6d7b
Gildardo Orozco <gorozco@csumb.edu>
2017-09-06 00:28:16 +0000

    index.php file modified

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Welcome Page!</title>
+    </head>
+    <body>
+        <a href="http://alftorres-cst336.herokuapp.com/labs/lab1/homePage.html" >Welcome!</a>
+    </body>
+</html>
